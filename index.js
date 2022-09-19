a9faaa0086c930d63638541c5a5ae332316f6b4f
ja54312 <ija54312@gmail.com>
2022-09-19 17:30:40 -0500

    primer server en express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('My app 2')
+//importamos la libreria express
+const express = require ('express')
+//creamos la app
+const app =  express()
+//puerto donde quieres lanzar tu app
+const port = 3000;
+//agregar una ruta
+app.get('/',(req,res)=>{
+  res.send('Hola mi server en express')
+})
+
+app.listen(port,()=>{
+  console.log('Mi port' + port)
+})
