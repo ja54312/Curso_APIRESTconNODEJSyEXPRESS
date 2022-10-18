31de62e12a956614bc23f2354d10c35d1879a270
ja54312 <ija54312@gmail.com>
2022-10-18 12:21:50 -0500

    deploy

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const routerApi = require ('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 //creamos la app
 const app =  express()
-//puerto donde quieres lanzar tu app
-const port = 3000;
+//puerto donde quieres lanzar tu app primero es la variable de entorno para prod y si no el puerto 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 //variable donde ingresamos solo los dominios que permitimos ingresar a nuestra api
-const whitelist = ['http://localhost:8080', 'https://myapp.co','http://127.0.0.1:5500','http://localhost:3000/'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co','http://127.0.0.1:5500'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
