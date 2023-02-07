5547a3205bd204d67b91f8fa7396119a833c86f2
ja54312 <ija54312@gmail.com>
2023-02-07 13:28:36 -0600

    desarrollo muchos a muchos

--- schemas/order.schema.js
+++ schemas/order.schema.js
@@ -1,23 +1,14 @@
 const Joi = require('joi');
 
 const id = Joi.number().integer();
-const email = Joi.string().email();
-const password = Joi.string().min(8);
-const role = Joi.string().min(5)
+const customerId = Joi.number().integer();
 
-const createUserSchema = Joi.object({
-  email: email.required(),
-  password: password.required(),
-  role: role.required()
-});
-
-const updateUserSchema = Joi.object({
-  email: email,
-  role: role,
-});
-
-const getUserSchema = Joi.object({
+const getOrderSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createUserSchema, updateUserSchema, getUserSchema }
+const createOrderSchema = Joi.object({
+  customerId: customerId.required(),
+});
+
+module.exports = { getOrderSchema, createOrderSchema };
