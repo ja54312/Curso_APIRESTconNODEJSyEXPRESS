f5c2e192e8630923a389b2dd58d52a09e7165d81
JA54312 <ija54312@gmail.com>
2023-04-10 13:32:37 -0600

    recovery mail sent

--- config/config.js
+++ config/config.js
@@ -7,6 +7,8 @@ const config = {
   dbUrl: process.env.DATABASE_URL,
   apikey:process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
+  smtpEmail: process.env.SMTP_EMAIL,
+  smtpPassword: process.env.SMTP_PASSWORD,
 }
 
 module.exports = { config };
